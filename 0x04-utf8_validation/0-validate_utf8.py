610928878e889d64acda85ceb83ee7436670b5ee
paxxchal <paschalgabrielacj8@gmail.com>
2024-08-23 05:56:24 +0100

    fixing an error 🛠️

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
 """
-UTF-8 Validation Module
+Module for UTF-8 validation
 """
 
 def validUTF8(data):
     """
-    Check if the data is a valid UTF-8 encoding.
-    :param data: List of integers
-    :return: True if data is valid UTF-8 encoding, else False
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    :param data: List of integers where each integer represents 1 byte of data
+    :return: True if data is a valid UTF-8 encoding, else False
     """
-    num_bytes = 0
+    # Number of bytes in the current UTF-8 character
+    n_bytes = 0
 
     for num in data:
-        # Get the 8 least significant bits
-        byte = num & 0xFF
+        # Get the 8 least significant bits of the number
+        byte = num & 255
 
-        if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7):
+        if n_bytes == 0:
+            # Determine how many bytes the UTF-8 character has
+            if byte >> 5 == 0b110:
+                n_bytes = 1
+            elif byte >> 4 == 0b1110:
+                n_bytes = 2
+            elif byte >> 3 == 0b11110:
+                n_bytes = 3
+            elif byte >> 7:
                 return False
         else:
-            # Check if the byte is a continuation byte
-            if (byte >> 6) != 0b10:
+            # Check if the byte is a valid continuation byte
+            if byte >> 6 != 0b10:
                 return False
-            num_bytes -= 1
+            n_bytes -= 1
 
-    return num_bytes == 0
-
-# Example usage
-if __name__ == "__main__":
-    data = [197, 130, 1]
-    print(validUTF8(data))  # Output: True
+    # All characters should be complete
+    return n_bytes == 0

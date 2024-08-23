2314d9b036247211bcdd218d0e380ae11e4f00e7
Paschal <59150676+paxxchal@users.noreply.github.com>
2024-08-23 05:59:43 +0100

    fixing an error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-"""
-Module for UTF-8 validation
-"""
+""" UTF-8 Validation """
+
 
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
-
-    :param data: List of integers where each integer represents 1 byte of data
-    :return: True if data is a valid UTF-8 encoding, else False
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
     """
-    # Number of bytes in the current UTF-8 character
-    n_bytes = 0
+    number_bytes = 0
 
-    for num in data:
-        # Get the 8 least significant bits of the number
-        byte = num & 255
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-        if n_bytes == 0:
-            # Determine how many bytes the UTF-8 character has
-            if byte >> 5 == 0b110:
-                n_bytes = 1
-            elif byte >> 4 == 0b1110:
-                n_bytes = 2
-            elif byte >> 3 == 0b11110:
-                n_bytes = 3
-            elif byte >> 7:
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
-            # Check if the byte is a valid continuation byte
-            if byte >> 6 != 0b10:
-                return False
-            n_bytes -= 1
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
 
-    # All characters should be complete
-    return n_bytes == 0
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False

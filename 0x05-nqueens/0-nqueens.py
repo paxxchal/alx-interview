53dc1b29ba08006162323b3557088b8baa60e43b
Paschal <59150676+paxxchal@users.noreply.github.com>
2024-09-01 23:18:09 +0100

    fixed an error 🛠️

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,56 +1,44 @@
 #!/usr/bin/python3
+""" N queens """
 import sys
 
-def is_safe(board, row, col):
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
 
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-    return True
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-def solve_n_queens_util(board, col, solutions):
-    if col >= len(board):
-        solutions.append([[i, board[i].index(1)] for i in range(len(board))])
-        return True
+n = int(sys.argv[1])
 
-    res = False
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            res = solve_n_queens_util(board, col + 1, solutions) or res
-            board[i][col] = 0
 
-    return res
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
-def solve_n_queens(n):
-    board = [[0] * n for _ in range(n)]
-    solutions = []
-    solve_n_queens_util(board, 0, solutions)
-    return solutions
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
 
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    solutions = solve_n_queens(n)
-    for solution in solutions:
-        print(solution)
+solve(n)
